e0c47cb1c4eb48051dec4344671d3f306aa79e93
Kody <kody11@gmail.com>
2022-11-13 01:36:13 -0700

    completed palindrome

--- 2022/9.palindrome-number.py
+++ 2022/9.palindrome-number.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x < 10:
+            return True
+        if x % 10 == 0:
+            return False
+        y = 0
+        while x > y:
+            y = y * 10 + x % 10
+            x = x // 10
+        return x == y or x == y // 10
         
 # @lc code=end
 
